f4c6d945a3d95fa06ab49a9cc54c34a13ee2b63c
Volodia P.-G. <volodia.parol-guarino@proton.me>
2025-04-09 10:36:25 +0200

    feat: add basic testing for speech_to_text

---
+++ lib/giraff/speech_to_text.ex
@@ -0,0 +1,34 @@
+defmodule Giraff.SpeechToText do
+  @moduledoc """
+  Speech to text module to be used by the Giraff app
+  """
+
+  require Logger
+  require OpenTelemetry.Tracer, as: Tracer
+
+  def speech_to_text(audio) when is_binary(audio) do
+    with transcription = {:ok, text} <- AI.SpeechRecognition.transcribe_audio(audio) do
+      Logger.debug("Got transcription #{inspect(text)}")
+      transcription
+    else
+      {:error, reason} ->
+        Tracer.add_event("speech_recognition.error", %{reason: reason})
+        Logger.error("Speech recognition failed: #{inspect(reason)}")
+        Tracer.add_event("speech_recognition_failed", %{error: inspect(reason)})
+        {:error, {:speech_recognition_failed, reason}}
+    end
+  end
+
+  @doc """
+  Outputs the settings to call the function on the appropriate FLAME backend
+  """
+  def remote_speech_to_text_spec(audio, opts) when is_binary(audio) do
+    Keyword.put_new(opts, :retries, 1)
+
+    [
+      Giraff.SpeechToTextBackend,
+      fn -> __MODULE__.speech_to_text(audio) end,
+      opts
+    ]
+  end
+end
---
+++ test/test_speech_to_text.exs
@@ -0,0 +1,31 @@
+defmodule SpeechToTextTest do
+  use ExUnit.Case
+  doctest FLAMERetry
+
+  require Logger
+
+  setup_all do
+    pid =
+      start_supervised!(
+        {FLAME.Pool,
+         name: Giraff.Test, backend: FLAME.LocalBackend, min: 0, max: 1, max_concurrency: 10}
+      )
+
+    path = Path.join([System.get_env("PATH_AUDIO"), "8842-304647-0007.wav"])
+    data = File.read!(path)
+
+    %{pool: pid, data: data}
+  end
+
+  test "nominal", %{data: data} do
+    transcription = Giraff.SpeechToText.speech_to_text(data)
+    assert {:ok, " I'm going to be the most wonderful."} == transcription
+  end
+
+  test "nominal with local backend", %{data: data} do
+    transcription =
+      apply(FLAME.Pool, :call, Giraff.SpeechToText.remote_speech_to_text_spec(data, []))
+
+    assert {:ok, " I'm going to be the most wonderful."} == transcription
+  end
+end
